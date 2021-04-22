d9718a60688e9cc36ec032666f0f03af27c7124c
carolemny <meneycarole@gmail.com>
2021-04-22 19:27:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,102 @@
+// -------------------------------------------------------------- const -------------------------------------------------------------------
+const displayResults = document.getElementById("movie");
+const errorDiv = document.getElementById("error");
+const searchBtn = document.getElementById("searchBtn");
+let apiKey = "";
+
+// ------------------------------------------------------------ functions -----------------------------------------------------------------
+
+// to get ApiKey
+const getApi = () => {
+  apiKey = prompt("Please enter your API Key");
+  console.log(apiKey);
+};
+
+// check if movie is in API
+const checkMovie = (data) => {
+  if (data.Response === "False") {
+    errorDiv.innerHTML += `
+    <div class="text-left text-danger m-3">
+      <p>Sorry, movie not found.</p>
+    </div>`;
+  } else {
+    errorDiv.innerHTML = "";
+  }
+};
+
+// displayMovie
+const displayMovie = (data) => {
+  console.log(data);
+  let date = data.Year;
+  let title = data.Title;
+  let picture = data.Poster;
+  let id = data.imdbID;
+  displayResults.innerHTML += `
+    <div class="row my-3 my-rounded d-flex justify-content-between border my-width-medium p-4">
+      <div class="col-3 d-flex">  
+        <img class="img-fluid" src="${picture}" alt="movie-poster">
+      </div>
+      <div class="col-9 d-flex flex-column justify-content-center">
+        <h2>${title.toUpperCase()}</h2>
+        <p><strong>${date}</strong></p>
+        <div>
+          <input class="btn btn-lg btn-primary my-rounded my-text my-3 my-width-medium" type="submit" value="Read more" id="${id}" onclick="readMore(id)"/>
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+// to open readMore
+const readMore = (data) => {
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${data}`)
+    .then((response) => response.json())
+    .then((response) => {
+      let displayContent = document.getElementById("popup-content");
+      let displaypopup = document.getElementById("popup");
+      displayContent.innerHTML = `
+        <div class="row">
+          <div class="col-3 d-flex justify-content-center align-items-center">  
+            <img src="${response.Poster}" alt="image-movie" class="img-fluid">
+          </div>
+          <div class="col-9 d-flex flex-column justify-content-center">
+            <h2>${response.Title.toUpperCase()}</h2>
+            <p><strong>${response.Released}</p></strong>
+            <p>${response.Plot}</p>
+          </div>
+        </div>
+      `;
+      displaypopup.style.display = "block";
+      document
+        .getElementById("closePopup")
+        .addEventListener("click", closePopup);
+    });
+};
+
+// to close readMore
+const closePopup = () => {
+  document.getElementById("popup").style.display = "none";
+};
+
+// fetching
+const fetching = () => {
+  let searchInput = document.getElementById("searchInput").value;
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput}`)
+    .then((response) => response.json())
+    .then((response) => {
+      checkMovie(response);
+      return response;
+    })
+    .then((response) => {
+      displayResults.innerHTML = "";
+      response.Search.forEach((element) => {
+        displayMovie(element);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+// -------------------------------------------------------------- listenner ---------------------------------------------------------------
+searchBtn.addEventListener("click", fetching);
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Projet API: FILMFINDER
+
+## Lancer le projet
+
+ * cloner le repo : `git clone https://github.com/carolemny/film_finder_api_project.git`
+ * lancer `index.html` dans un navigateur
+
+## Lien du site en production
+
+[STUDIO](https://filmfindercarolemny.herokuapp.com/)
+
+## Détail des fonctionnalités
+
+Ce que contient le **projet** :
+
+* L'utilisateur peut chercher une liste de films/séries grâce à des mots clefs
+* L'utilisateur verra les films/séries apparaître sous forme de blocs, contenant le nom du film, la date de parution et une image ainsi qu'un CTA contenant "Read More".
+* L'utilisateur peut cliquer sur le CTA "Read More" afin d'afficher la description du film ainsi que sa date de parution dans une popup.
+* Les films/séries apparaissent petit à petit, au fur et à mesure du scroll.
+
+
+## L'équipe
+
+* **Carole Meney** _alias_ [@carolemny](https://github.com/carolemny)
